f5c6b4dd80295e77542e6a6eb8bb5ff8d75f0e96
ArtemHard <95387501+ArtemHard@users.noreply.github.com>
2023-07-27 03:02:59 +0300

    create input, add icons, showPassword clickHandler, styles

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,7 +1,9 @@
-import { ComponentPropsWithoutRef, forwardRef } from 'react'
+import { ComponentPropsWithoutRef, forwardRef, useState } from 'react'
 
 import clsx from 'clsx'
 
+import { EyeIconSvg } from '../../../assets/icons/EyeIconSvg'
+import EyeOff from '../../../assets/icons/EyeOff'
 import { Typography } from '../Typography'
 
 import style from './Input.module.scss'
@@ -13,18 +15,37 @@ export type InputProps = {
 } & ComponentPropsWithoutRef<'input'>
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, errorMessage, placeholder, type, label, onChange, ...restProps }, ref) => {
+  ({ className, errorMessage, placeholder, type = 'text', label, onChange, ...restProps }, ref) => {
     const classNames = {
       root: style.root,
-      input: clsx(className, style.input),
-      label: style.label,
+      input: clsx(className, style.input, !!errorMessage && style.error),
     }
+    const onClickShowIconHandler = () => setShowIcon(prev => !prev)
+
+    const [showIcon, setShowIcon] = useState(true)
 
     return (
       <div className={classNames.root}>
-        <Typography variant="caption">{label}</Typography>
-        <input type="text" name="" className={classNames.input} placeholder={placeholder} />
+        <Typography className={style.label}>{label}</Typography>
+        <input
+          type={generateType(type, showIcon)}
+          ref={ref}
+          className={classNames.input}
+          placeholder={placeholder}
+        />
+        {type === 'password' && (
+          <button type="button" className={style.rightIcon} onClick={onClickShowIconHandler}>
+            {showIcon ? <EyeIconSvg /> : <EyeOff />}
+          </button>
+        )}
+        {errorMessage && <Typography className={style.errorLabel}>{errorMessage}</Typography>}
       </div>
     )
   }
 )
+
+const generateType = (type: React.HTMLInputTypeAttribute, showIcon: boolean) => {
+  if (type === 'password' && showIcon) {
+    return 'password'
+  } else 'text'
+}
