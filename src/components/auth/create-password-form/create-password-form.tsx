9c9165bea601167aa7b7b4701270f60fe72b4aeb
ArtemHard <95387501+ArtemHard@users.noreply.github.com>
2023-08-01 23:55:17 +0300

    create newPassword form, add styles

---
+++ src/components/auth/create-password-form/create-password-form.tsx
@@ -0,0 +1,38 @@
+import { Button } from '../../ui/button'
+import { Card } from '../../ui/card'
+import { Input } from '../../ui/Input'
+import { Typography } from '../../ui/Typography'
+
+import { CreatePasswordFormProps, useCreatePasswordForm } from './create-password-form'
+import style from './create-password-form.module.scss'
+
+export const CreatePasswordForm = (onSubmit: CreatePasswordFormProps) => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useCreatePasswordForm(onSubmit)
+
+  return (
+    <Card className={style.card}>
+      <Typography as="h1" variant="h1" className={style.title}>
+        Create new password
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <Input
+          {...register('password')}
+          errorMessage={errors.password?.message}
+          type="password"
+          label={'Password'}
+          className={style.textField}
+        />
+        <Typography variant="body2" className={style.text}>
+          Create new password and we will send you further instructions to email
+        </Typography>
+        <Button type="submit" fullWidth className={style.button}>
+          Create New Password
+        </Button>
+      </form>
+    </Card>
+  )
+}
