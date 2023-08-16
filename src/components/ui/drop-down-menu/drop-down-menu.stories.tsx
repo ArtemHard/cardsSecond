24635a40dfdecbc975cd00585b9db994f93072c2
Artem <95387501+ArtemHard@users.noreply.github.com>
2023-08-16 19:07:07 +0000

    31 create drop down menu (#44)
    * create drop down, add arrow, decompose components
    
    * add lines add default story as figma, add Profile Icon, delete arrow in dropdown menu
    
    * finish dropdown without arrow #44  #43 #31 #42

---
+++ src/components/ui/drop-down-menu/drop-down-menu.stories.tsx
@@ -0,0 +1,69 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { EditPenSvg, LogoutSvg } from '../../../assets/icons'
+import { PersonOutlineSvg } from '../../../assets/icons/PersonOutlineSvg'
+import { Avatar } from '../avatar'
+import Button from '../button/button'
+import { Typography } from '../Typography'
+
+import { DropDownMenu, DropDownMenuIcon } from './drop-down-menu'
+
+const meta = {
+  title: 'Components/DropDownMenu',
+  component: DropDownMenu,
+  tags: ['autodocs'],
+} satisfies Meta<typeof DropDownMenu>
+
+export default meta
+// type Story = StoryObj<typeof meta>
+
+export const Variants = {
+  render: () => {
+    return (
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+        <DropDownMenu trigger={<Button>clik 1111111111111111</Button>}>
+          <DropDownMenuIcon onSelect={() => {}}>
+            <div
+              style={{ display: 'flex', flexDirection: 'row', gap: '0.5rem', alignItems: 'center' }}
+            >
+              <Avatar name="Ivan" />
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                <Typography variant="subtitle2" as="span">
+                  Ivan
+                </Typography>
+                <Typography variant="caption" as="span">
+                  j&johnson@gmail.com
+                </Typography>
+              </div>
+            </div>
+          </DropDownMenuIcon>
+          <DropDownMenuIcon onSelect={() => {}} icon={<PersonOutlineSvg />}>
+            My Profile
+          </DropDownMenuIcon>
+          <DropDownMenuIcon icon={<LogoutSvg />}>Sign Out</DropDownMenuIcon>
+        </DropDownMenu>
+        <DropDownMenu trigger={<Avatar name="Ivan" />}>
+          <DropDownMenuIcon onSelect={() => {}}>
+            <div
+              style={{ display: 'flex', flexDirection: 'row', gap: '0.5rem', alignItems: 'center' }}
+            >
+              <Avatar name="Ivan" />
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                <Typography variant="subtitle2" as="span">
+                  Ivan
+                </Typography>
+                <Typography variant="caption" as="span">
+                  j&johnson@gmail.com
+                </Typography>
+              </div>
+            </div>
+          </DropDownMenuIcon>
+          <DropDownMenuIcon onSelect={() => {}} icon={<PersonOutlineSvg />}>
+            My Profile
+          </DropDownMenuIcon>
+          <DropDownMenuIcon icon={<LogoutSvg />}>Sign Out</DropDownMenuIcon>
+        </DropDownMenu>
+      </div>
+    )
+  },
+}
---
+++ src/components/ui/drop-down-menu/drop-down-menu.tsx
@@ -0,0 +1,80 @@
+import { ReactNode, useState, FC, CSSProperties } from 'react'
+
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+
+import { Typography } from '../Typography'
+
+import style from './drop-down-menu.module.scss'
+
+export type DropdownProps = {
+  children: ReactNode
+  align?: 'start' | 'center' | 'end'
+  trigger?: ReactNode
+  className?: string
+  style?: CSSProperties
+} & DropdownMenu.DropdownMenuProps
+
+export const DropDownMenu: FC<DropdownProps> = ({
+  children,
+  align,
+  className,
+  trigger,
+  ...restProps
+}) => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <DropdownMenu.Root onOpenChange={setOpen} modal>
+      <DropdownMenu.Trigger asChild>
+        <div style={{ height: 'fit-content', width: 'fit-content', position: 'relative' }}>
+          {trigger}
+          {open && <div className={style.DropdownMenuArrow3}></div>}
+        </div>
+      </DropdownMenu.Trigger>
+      {/* forceMount Useful when controlling animation */}
+
+      <DropdownMenu.Portal className={style.portal}>
+        <DropdownMenu.Content
+          className={style.DropdownMenuContent}
+          align="start"
+          sideOffset={5}
+          hideWhenDetached
+        >
+          {children}
+          {/* <DropdownMenu.Arrow className={style['DropdownMenuArrow']} /> */}
+          {/* <div className={style.DropdownMenuArrow2}></div> */}
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  )
+}
+
+type DropDownMenuIconProps = {
+  children: ReactNode
+  icon?: ReactNode
+} & DropdownMenu.DropdownMenuItemProps
+export const DropDownMenuIcon = ({
+  onSelect,
+  disabled,
+  className,
+  children,
+  icon,
+  ...restProps
+}: DropDownMenuIconProps) => {
+  return (
+    <>
+      <DropdownMenu.Item
+        onSelect={onSelect}
+        // className={className}
+        className={style.DropdownMenuItem}
+        disabled={disabled}
+        {...restProps}
+      >
+        {icon}
+        <Typography variant="subtitle2" style={{ margin: 0 }}>
+          {children}
+        </Typography>
+      </DropdownMenu.Item>
+    </>
+  )
+}
