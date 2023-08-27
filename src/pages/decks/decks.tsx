836dfdd627be395c6b5bbe1a1c74f2621fcb6e88
Artem <95387501+ArtemHard@users.noreply.github.com>
2023-08-27 19:10:22 +0300

    57 deck api (#62)
    * add optimistic update for delete and create deck in Api
    
    * fix bags with logout, add change avatar, name handlers, fix style in avatar
    
    * create decks filter components, add style

---
+++ src/pages/decks/decks.tsx
@@ -0,0 +1,11 @@
+import { DecksFilter } from '../../components/decks/decks-filters/decks-filter'
+
+import style from './decks.style.module.scss'
+
+export const Decks = () => {
+  return (
+    <>
+      <DecksFilter />
+    </>
+  )
+}
