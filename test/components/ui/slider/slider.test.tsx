132ab70ed088e5932074201f8249b616de4510e1
ArtemHard <95387501+ArtemHard@users.noreply.github.com>
2023-10-13 23:55:23 +0300

    fix test

--- test/components/ui/slider/slider.test.tsx
+++ test/components/ui/slider/slider.test.tsx
@@ -1,41 +1,44 @@
 // eslint-disable-next-line import/default
 import React from 'react'
 
-import { render, screen } from '@testing-library/react'
-import { afterEach, beforeEach, expect, describe, test, vi } from 'vitest'
+import { RenderResult, render, screen } from '@testing-library/react'
+import { afterEach, beforeEach, expect, describe, test, vi, it } from 'vitest'
 
 import { Slider } from '../../../../src/components/ui/slider/index'
 
-describe('test for Slider', () => {
+describe('test of basic params', () => {
   const sliderId = 'ajrgkjadbqdegbkjdsfg'
+  const defaultNumbers = [0, 12]
+  let slider: HTMLDivElement
 
-  describe('test of basic params', () => {
-    const defaultNumbers = [0, 100]
-    let slider: HTMLDivElement
+  beforeEach(() => {
+    const ResizeObserverMock = vi.fn(() => ({
+      observe: vi.fn(),
+      unobserve: vi.fn(),
+      disconnect: vi.fn(),
+    }))
 
-    beforeEach(() => {
-      const ResizeObserverMock = vi.fn(() => ({
-        observe: vi.fn(),
-        unobserve: vi.fn(),
-        disconnect: vi.fn(),
-      }))
+    vi.stubGlobal('ResizeObserver', ResizeObserverMock)
+    render(<Slider data-testid={sliderId} defaultValue={defaultNumbers} value={defaultNumbers} />)
+    slider = screen.getByTestId(sliderId)
+  })
 
-      vi.stubGlobal('ResizeObserver', ResizeObserverMock)
-      render(<Slider data-testid={sliderId} defaultValue={defaultNumbers} />)
-      slider = screen.getByTestId(sliderId)
-    })
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
 
-    afterEach(() => {
-      vi.clearAllMocks()
-    })
+  test('check default value', () => {
+    const minValueElement = screen.getByLabelText('minValue').innerHTML
+    const maxValueElement = screen.getByLabelText('maxValue').innerHTML
 
-    test('check default value', () => {
-      const elems = Array.prototype.slice.call(slider.childNodes)
+    expect(minValueElement).toBe(defaultNumbers[0].toString())
+    expect(maxValueElement).toBe(defaultNumbers[1].toString())
+    expect(slider.innerHTML).toBeDefined()
+  })
 
-      //   elems.forEach(function (elem) {
-      //     console.log(elem) // HEAD, текст, BODY
-      //   })
-      expect(slider.innerHTML).toBeDefined()
-    })
+  it('snap shot mathes', () => {
+    const wrapper = render(<Slider />)
+
+    expect(wrapper).toMatchSnapshot()
   })
 })
